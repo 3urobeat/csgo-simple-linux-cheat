f74dc522aca56d28bdc6b8dd866f907ea292b620
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-02-16 19:43:41 +0100

    Add more interfaces which are needed later

--- src/SDK/interfaces/files/iBaseClientDLL.h
+++ src/SDK/interfaces/files/iBaseClientDLL.h
@@ -32,6 +32,53 @@ struct RecvTable {
 };
 
 
+struct DVariant {
+	union {
+		float m_Float;
+		long m_Int;
+		char *m_pString;
+		void *m_pData;
+		float m_Vector[3];
+		int64_t m_Int64;
+	};
+
+	int m_Type;
+};
+
+
+// https://github.com/ValveSoftware/source-sdk-2013/blob/0d8dceea4310fde5706b3ce1c70609d72a38efdf/sp/src/public/dt_recv.h#L28
+struct CRecvProxyData {
+	const RecvProp* m_pRecvProp;
+	DVariant m_Value;
+	int m_iElement;
+	int m_ObjectID;
+};
+
+
+// https://github.com/ValveSoftware/source-sdk-2013/blob/0d8dceea4310fde5706b3ce1c70609d72a38efdf/sp/src/public/dt_recv.h#L41
+typedef void (*RecvVarProxyFn) (const CRecvProxyData *pData, void *pStruct, void *pOut);
+
+
+// https://github.com/ValveSoftware/source-sdk-2013/blob/0d8dceea4310fde5706b3ce1c70609d72a38efdf/mp/src/public/dt_recv.h#L87
+struct RecvProp {
+	char *m_pVarName;
+	int m_RecvType;
+	int m_Flags;
+	int m_StringBufferSize;
+	bool m_bInsideArray;
+	const void *m_pExtraData;
+	RecvProp *m_pArrayProp;
+	void *m_ArrayLengthProxy;
+	RecvVarProxyFn m_ProxyFn;
+	void *m_DataTableProxyFn;
+	RecvTable *m_pDataTable;
+	int m_Offset;
+	int m_ElementStride;
+	int m_nElements;
+	const char *m_pParentArrayPropName;
+};
+
+
 // https://github.com/ValveSoftware/source-sdk-2013/blob/0d8dceea4310fde5706b3ce1c70609d72a38efdf/mp/src/public/client_class.h#L38
 class IClientNetworkable;
 typedef IClientNetworkable* (*CreateClientClassFn)(int entnum, int serialNum);
